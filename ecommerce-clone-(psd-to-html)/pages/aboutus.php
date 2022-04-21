9e44a5251a492b566def6dfd003880d0390f59c1
Jaffar Abbas <gamaportal8@gmail.com>
2022-04-21 16:53:36 +0500

    21/04/2022 16:53:36

---
+++ ecommerce-clone-(psd-to-html)/pages/aboutus.php
@@ -0,0 +1,44 @@
+<?php include("header.php") ?>
+    <!-- banner start -->
+    <section class="about_banner">
+        <div class="container">
+            <!-- col-sm-6 col-sm-offset-2 col-xs-6 col-xs-offset-1 -->
+            <div class="row mini_banner_row">
+                <div class="col-md-6 col-md-offset-3">
+                    <div class="mini_banner_center text-center">
+                        <h1>About Us</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- banner end -->
+
+    <!-- about start -->
+    <section class="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="about_side_1">
+                        <h3 class="primaryHeading">About us</h3>
+                        <h1 class="secondaryHeading">Where Class</br>Meets Glamour</h1>
+                        <p class="primaryParagraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        </p>
+                        <p class="primaryParagraph">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis
+                            unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+                        </p>
+                        <a href="" class="btnStyle">Learn More</a>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="about_side_2">
+                        <img src="../vendor/images/aboutPic1.png" alt="about pic 1">
+                        <img src="../vendor/images/aboutPic2.png" alt="about pic 2">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- about end -->
+<!-- contact end -->
+<?php include("footer.php") ?>
