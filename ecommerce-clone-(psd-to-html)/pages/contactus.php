94889d93846bab1d11710668a0d4fcdfc4b13dc4
Jaffar Abbas <gamaportal8@gmail.com>
2022-04-21 16:53:36 +0500

    21/04/2022 16:53:36

---
+++ ecommerce-clone-(psd-to-html)/pages/contactus.php
@@ -0,0 +1,59 @@
+<?php include("header.php") ?>
+
+<!-- banner start -->
+<section class="contact_banner">
+    <div class="container">
+        <div class="row mini_banner_row">
+            <div class="col-md-6 col-md-offset-3">
+                <div class="mini_banner_center text-center">
+                    <h1>Contact Us</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- banner end -->
+
+<!-- about start -->
+<section class="about">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="about_side_1">
+                            <h3 class="primaryHeading">Contact us</h3>
+                            <h1 class="secondaryHeading">Any Query</br>Please Tell Us</h1>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="maincontactfeilds">
+                            <form>
+                                <input class="contactInput" type="text" placeholder="Please enter your name"><br>
+                                <input class="contactInput" type="email" placeholder="Please enter your email address"></br>
+                                <div class="minicontactfeilds">
+                                    <input class="minicontactInput" type="text" placeholder="Please enter your phone">
+                                    <input class="minicontactInput" type="text" placeholder="Please enter your address">
+                                </div>
+                                <textarea class="contactInput" placeholder="Please enter your Message" name="" rows="3"></textarea><br>
+                                <input type="submit" class="btnStyle" value="Subscribe">
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-12">
+                <class class="contact_side_2">
+                    <div class="map">
+                        <iframe src="https://maps.google.com/maps?q=rizvia&t=k&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+                    </div>
+                </class>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- about end -->
+
+<?php include("footer.php") ?>
