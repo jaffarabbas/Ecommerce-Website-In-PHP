1462ef3aca879a0c38f354c0fd3bb4c588d91b97
Jaffar Abbas <gamaportal8@gmail.com>
2022-04-21 16:53:36 +0500

    21/04/2022 16:53:36

---
+++ ecommerce-clone-(psd-to-html)/pages/index.php
@@ -0,0 +1,187 @@
+<?php include("header.php") ?>
+<!-- banner start -->
+<section class="banner">
+    <div id="productCarousel" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#productCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#productCarousel" data-slide-to="1"></li>
+            <li data-target="#productCarousel" data-slide-to="2"></li>
+        </ol>
+
+        <div class="carousel-inner" role="listbox">
+            <div class="item active">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6 col-md-offset-3">
+                            <div class="banner_center text-center">
+                                <h1>Your Jewlery Is Our Passion</h1>
+                                <p class="primaryParagraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                    consequat. </p>
+                                <a href="">Learn More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6 col-md-offset-3">
+                            <div class="banner_center text-center">
+                                <h1>All Jewlery and Its Services</h1>
+                                <p class="primaryParagraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                    consequat. </p>
+                                <a href="">Learn More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6 col-md-offset-3">
+                            <div class="banner_center text-center">
+                                <h1>Best Jewlery In Town</h1>
+                                <p class="primaryParagraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                    consequat. </p>
+                                <a href="">Learn More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <a class="left carousel-control" href="#productCarousel" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#productCarousel" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+</section>
+<!-- banner end -->
+
+<!-- about start -->
+<section class="about">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="about_side_1">
+                    <h3 class="primaryHeading">About us</h3>
+                    <h1 class="secondaryHeading">Where Class</br>Meets Glamour</h1>
+                    <p class="primaryParagraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                    <p class="primaryParagraph">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis
+                        unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+                    </p>
+                    <a href="" class="btnStyle">Learn More</a>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="about_side_2">
+                    <img src="../vendor/images/aboutPic1.png" alt="about pic 1">
+                    <img src="../vendor/images/aboutPic2.png" alt="about pic 2">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- about end -->
+<!-- product start -->
+<section class="product">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="productHeader">
+                    <h3 class="primaryHeading">Product</h3>
+                    <h1 class="secondaryHeading">Our Product</h1>
+                    <p class="primaryParagraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+                </div>
+            </div>
+        </div>
+        <div class="row prrow">
+            <div class="col-md-3 col-sm-6 col-xs-6">
+                <img src="../vendor/images/productPic1.png" alt="product pic 1">
+                <p>your Heading here</p>
+            </div>
+            <div class="col-md-3 col-sm-6 col-xs-6">
+                <img src="../vendor/images/productPic2.png" alt="product pic 1">
+                <p>your Heading here</p>
+            </div>
+            <div class="col-md-3 col-sm-6 col-xs-6">
+                <img src="../vendor/images/productPic3.png" alt="product pic 1">
+                <p>your Heading here</p>
+            </div>
+            <div class="col-md-3 col-sm-6 col-xs-6">
+                <img src="../vendor/images/productPic4.png" alt="product pic 1">
+                <p>your Heading here</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="productFooter">
+                    <a class="miniButton" href="">View All</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- product end -->
+
+<!-- categories start -->
+<section class="categories">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="categoriesHeader">
+                    <h3 class="primaryHeading">Categories</h3>
+                    <h1 class="secondaryHeading">Product By Categories</h1>
+                    <p class="primaryParagraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4 col-sm-6">
+                <img src="../vendor/images/categoriesPic1.png" alt="categories pic 1">
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <img src="../vendor/images/categoriesPic2.png" alt="categories pic 2">
+            </div>
+            <div class="col-md-4 col-md-offset-0 col-sm-6 col-sm-offset-3">
+                <img src="../vendor/images/categoriesPic3.png" alt="categories pic 3">
+            </div>
+        </div>
+    </div>
+</section>
+<!-- categories end -->
+
+<!-- contact start -->
+<section class="contact">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="contactHeader">
+                    <h1 class="secondaryHeading">Subscribe Our news Letter</h1>
+                    <p class="primaryParagraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="contactfeilds">
+                    <form>
+                        <input class="contactInput" type="email" placeholder="Please enter your email address">
+                        <input type="submit" class="btnStyle" value="Subscribe">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- contact end -->
+<?php include("footer.php") ?>
