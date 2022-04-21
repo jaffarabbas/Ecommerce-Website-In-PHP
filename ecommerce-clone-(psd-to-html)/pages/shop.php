11576667990f1b9c77320bab4627a59165339ea9
Jaffar Abbas <gamaportal8@gmail.com>
2022-04-21 16:53:37 +0500

    21/04/2022 16:53:36

---
+++ ecommerce-clone-(psd-to-html)/pages/shop.php
@@ -0,0 +1,172 @@
+<?php include("header.php") ?>
+
+<!-- banner start -->
+<section class="shop_banner">
+    <div class="container">
+        <div class="row mini_banner_row">
+            <div class="col-md-6 col-md-offset-3">
+                <div class="mini_banner_center text-center">
+                    <h1>Shop</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- banner end -->
+
+<!-- product start -->
+<section class="productPage">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="productHeader">
+                    <h3 class="primaryHeading">Product</h3>
+                    <h1 class="secondaryHeading">Our Product</h1>
+                    <p class="primaryParagraph">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="shopmenu">
+                    <a data-toggle="tab" href="#shpprcr1" class="active btnStyle">Latest Products</a>
+                    <a data-toggle="tab" href="#shpprcr2" class="btnStyle">Our Premium</a>
+                    <a data-toggle="tab" href="#shpprcr3" class="btnStyle">Artifical Products</a>
+                </div>
+            </div>
+        </div>
+        <div class="tab-content">
+            <div id="shpprcr1" class="tab-pane fade in active">
+                <div class="row prrow">
+                    <div class="col-md-3 col-sm-6 col-xs-6">
+                        <a href="productDetail.php">
+                            <img src="../vendor/images/productPic1.png" alt="product pic 1" />
+                            <p>your Heading here<br />your Heading here</p>
+                        </a>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-xs-6">
+                        <a href="productDetail.php">
+                            <img src="../vendor/images/productPic2.png" alt="product pic 1" />
+                            <p>your Heading here<br />your Heading here</p>
+                        </a>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-xs-6">
+                        <a href="productDetail.php">
+                            <img src="../vendor/images/productPic3.png" alt="product pic 1" />
+                            <p>your Heading here<br />your Heading here</p>
+                        </a>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-xs-6">
+                        <a href="productDetail.php">
+                            <img src="../vendor/images/productPic4.png" alt="product pic 1" />
+                            <p>your Heading here<br />your Heading here</p>
+                        </a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="pagination">
+                            <a href="#">&laquo;</a>
+                            <a href="#">1</a>
+                            <a href="#">2</a>
+                            <a href="#">3</a>
+                            <a href="#" class="active">4</a>
+                            <a href="#">5</a>
+                            <a href="#">6</a>
+                            <a href="#">&raquo;</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="shpprcr2" class="tab-pane fade">
+                <div class="row prrow">
+                    <div class="col-md-3 col-sm-6 col-xs-6">
+                        <a href="productDetail.php">
+                            <img src="../vendor/images/productPic1.png" alt="product pic 1" />
+                            <p>your Heading here<br />your Heading here</p>
+                        </a>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-xs-6">
+                        <a href="productDetail.php">
+                            <img src="../vendor/images/productPic2.png" alt="product pic 1" />
+                            <p>your Heading here<br />your Heading here</p>
+                        </a>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-xs-6">
+                        <a href="productDetail.php">
+                            <img src="../vendor/images/productPic3.png" alt="product pic 1" />
+                            <p>your Heading here<br />your Heading here</p>
+                        </a>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-xs-6">
+                        <a href="productDetail.php">
+                            <img src="../vendor/images/productPic4.png" alt="product pic 1" />
+                            <p>your Heading here<br />your Heading here</p>
+                        </a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="pagination">
+                            <a href="#">&laquo;</a>
+                            <a href="#">1</a>
+                            <a href="#">2</a>
+                            <a href="#">3</a>
+                            <a href="#" class="active">4</a>
+                            <a href="#">5</a>
+                            <a href="#">6</a>
+                            <a href="#">&raquo;</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="shpprcr3" class="tab-pane fade">
+                <div class="row prrow">
+                    <div class="col-md-3 col-sm-6 col-xs-6">
+                        <a href="productDetail.php">
+                            <img src="../vendor/images/productPic1.png" alt="product pic 1" />
+                            <p>your Heading here<br />your Heading here</p>
+                        </a>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-xs-6">
+                        <a href="productDetail.php">
+                            <img src="../vendor/images/productPic2.png" alt="product pic 1" />
+                            <p>your Heading here<br />your Heading here</p>
+                        </a>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-xs-6">
+                        <a href="productDetail.php">
+                            <img src="../vendor/images/productPic3.png" alt="product pic 1" />
+                            <p>your Heading here<br />your Heading here</p>
+                        </a>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-xs-6">
+                        <a href="productDetail.php">
+                            <img src="../vendor/images/productPic4.png" alt="product pic 1" />
+                            <p>your Heading here<br />your Heading here</p>
+                        </a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="pagination">
+                            <a href="#">&laquo;</a>
+                            <a href="#">1</a>
+                            <a href="#">2</a>
+                            <a href="#">3</a>
+                            <a href="#" class="active">4</a>
+                            <a href="#">5</a>
+                            <a href="#">6</a>
+                            <a href="#">&raquo;</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- product end -->
+
+<?php include("footer.php") ?>
