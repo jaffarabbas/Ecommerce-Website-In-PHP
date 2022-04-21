00ed6886a5fbff58c323160b1044aeaf960b99be
Jaffar Abbas <gamaportal8@gmail.com>
2022-04-21 16:53:37 +0500

    21/04/2022 16:53:36

---
+++ ecommerce-clone-(psd-to-html)/pages/login.php
@@ -0,0 +1,39 @@
+<?php include("header.php") ?>
+
+<!-- banner start -->
+<section class="login_banner">
+    <div class="container">
+        <div class="row mini_banner_row">
+            <div class="col-md-6 col-md-offset-3">
+                <div class="mini_banner_center text-center">
+                    <h1>Login</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- banner end -->
+
+<!-- product start -->
+<section class="authPage">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 col-xs-12">
+                <div class="auth">
+                    <form>
+                        <h1>Login</h1>
+                        <input class="contactInput" type="email" placeholder="Email Address"><br>
+                        <input class="contactInput" type="text" placeholder="Password"><br>
+                        <input type="submit" class="btnStyle" value="Login">
+                        <br>
+                        <br>
+                        <a href="signup.php">Create new Account</a>&nbsp;&nbsp;&nbsp;<a href="">Forget passowrd ?</a>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- product end -->
+
+<?php include("footer.php") ?>
