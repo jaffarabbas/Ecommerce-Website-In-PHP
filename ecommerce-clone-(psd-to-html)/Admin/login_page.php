15aca25cf451bbca9af27ae7dc2331716fd16e42
Jaffar Abbas <gamaportal8@gmail.com>
2022-04-22 19:16:16 +0500

    22/04/2022 19:16:15

---
+++ ecommerce-clone-(psd-to-html)/Admin/login_page.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LOGIN</title>
+	<link rel="stylesheet" type="text/css" href="../vendor/css/adminStyle.css">
+
+	<link href="https://fonts.googleapis.com/css2?family=Odibee+Sans&display=swap" rel="stylesheet">
+</head>
+<body>
+<div ><p Id="FAJ_LOGO">PERL JEWL ADMIN SYSTEM</p></div>
+     <form action="login.php" method="post">
+     	<h2>LOGIN</h2>
+     	<?php if (isset($_GET['error'])) { ?>
+     		<p class="error"><?php echo $_GET['error']; ?></p>
+     	<?php } ?>
+     	<label>User Name</label>
+     	<input type="text" name="username" placeholder="User Name"><br>
+
+     	<label>Password</label>
+     	<input type="password" name="password" placeholder="Password"><br>
+
+		
+		 <button type="submit">Login</button>
+		 <a href="contact.php" class="contact" id="con_btn" >Contact us</a>
+	 </form>
+</body>
+</html>
