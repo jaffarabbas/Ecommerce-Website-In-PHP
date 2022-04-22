06cff968b925ce21b301425156b9016287e9cd57
Jaffar Abbas <gamaportal8@gmail.com>
2022-04-22 19:16:16 +0500

    22/04/2022 19:16:15

---
+++ ecommerce-clone-(psd-to-html)/Admin/footer.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin panel</title>
+
+    <!-- BOOTSTRAP STYLES-->
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+    <link href="../vendor/css/css.css" rel="stylesheet" />
+    <link href="../vendor/bootstrap/css/bootstrap.css" rel="stylesheet" />
+
+    <link href="../vendor/css/custom.css" rel="stylesheet" />
+    <link href="../vendor/css/font-awesome.css" rel="stylesheet" />
+
+    <link rel="stylesheet" href="//cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">
+
+</head>
+
+<body>
+
+    </section>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+</body>
+<script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+<script src="../vendor/js/jquery-1.10.2.js"></script>
+<script src="../vendor/js/jquery.metisMenu.js"></script>
+<script src="//cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+<script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="../vendor/js/custom.js"></script>
+
+
+<script>
+    $(document).ready(function() {
+        $('#myTable').DataTable();
+    });
+    $(document).ready(function() {
+        $('#myTable2').DataTable();
+    });
+    $(document).ready(function() {
+        $('#myTable3').DataTable();
+    });
+    $(document).ready(function() {
+        $('#myTable4').DataTable();
+    });
+</script>
+
+</html>
