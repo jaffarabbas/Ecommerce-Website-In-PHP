7c3ce9c9f856cac5cee74de9b180cfa68a5b3f70
Jaffar Abbas <gamaportal8@gmail.com>
2022-04-24 19:26:01 +0500

    24/04/2022 19:26:00

--- ecommerce-clone-(psd-to-html)/vendor/js/index.js
+++ ecommerce-clone-(psd-to-html)/vendor/js/index.js
@@ -1,18 +1,30 @@
 /*Quantity incrementer */
-var counter = 0;
+var counter = 1;
 
 function inputValue() {
     document.getElementById("quantityInc").value = counter;
 }
 
+//set counter value through out the website
+function checkCounter() {
+    var val = document.getElementById("quantityInc").value;
+    val > 0 ? counter = val : counter = 0;
+}
+
 function incrementer() {
+    checkCounter();
     counter++;
     inputValue();
+    //update total price
+    subTotal();
 }
 
 function decrementer() {
+    checkCounter();
     counter > 0 ? counter-- : counter;
     inputValue();
+    //update total price
+    subTotal();
 }
 
 /*product deitals img slider */
