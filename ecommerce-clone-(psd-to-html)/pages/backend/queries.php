12f7eea170d3b4c303b2836bd56561da84e8cd73
Jaffar Abbas <gamaportal8@gmail.com>
2022-04-24 19:26:01 +0500

    24/04/2022 19:26:00

--- ecommerce-clone-(psd-to-html)/pages/backend/queries.php
+++ ecommerce-clone-(psd-to-html)/pages/backend/queries.php
@@ -2,5 +2,10 @@
 class Queries{
     public static string $getAllProductsByCategories = "SELECT `pid`, `name`, `description`, `price`, JSON_EXTRACT(image,'$[0]') as fimage, `quantity`, `rating`, `cid` FROM `products` WHERE `cid` =";
     public static string $getProductByPId = "SELECT `pid`, `name`, `description`, `price`, `image` , `quantity`, `rating`, `cid` FROM `products` WHERE `pid` = ";
+    public static string $getTaxtFromSetting = "SELECT `TAX` FROM `setting`";
+    public static string $getAllAccountType = "SELECT * FROM `account_type`";
+    public static string $getAllType = "SELECT * FROM `type`";
+    public static string $checkSameEmail = "SELECT * FROM users WHERE email = ";
+    public static string $insertUser = "INSERT INTO users (firstname, lastname, email, password, actype, type) VALUES (?,?,?,?,?,?)";
 }
 ?>
