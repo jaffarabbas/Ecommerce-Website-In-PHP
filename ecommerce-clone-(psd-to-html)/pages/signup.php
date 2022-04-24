69e040c9ec8989b67656acc0b568a377efe27c2d
Jaffar Abbas <gamaportal8@gmail.com>
2022-04-24 19:26:01 +0500

    24/04/2022 19:26:00

--- ecommerce-clone-(psd-to-html)/pages/signup.php
+++ ecommerce-clone-(psd-to-html)/pages/signup.php
@@ -1,5 +1,15 @@
 <?php include("header.php") ?>
 
+<?php
+
+include_once("./backend/connecter.php");
+
+$result1 = $operations->getData(Queries::$getAllAccountType);
+$result2 = $operations->getData(Queries::$getAllType);
+
+?>
+
+
 <!-- banner start -->
 <section class="login_banner">
     <div class="container">
@@ -20,37 +30,47 @@
         <div class="row">
             <div class="col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 col-xs-12">
                 <div class="auth">
-                    <form>
+                    <form action="./backend/signup_bakend.php" method="POST">
                         <h1>Signup</h1>
+                        <?php
+                        if (isset($_SESSION['error']) && $_SESSION['error'] != "") {
+                            echo $_SESSION['error'];
+                            unset($_SESSION['error']);
+                        }elseif(isset($_SESSION['success']) && $_SESSION['success'] != ""){
+                            echo $_SESSION['success'];
+                            unset($_SESSION['success']);
+                        }
+                        ?>
                         <div class="act">
                             <p>Create</p>
-                            <select class="contactInput">
+                            <select name="actype" class="contactInput">
                                 <option hidden="true">Select Account</option>
                                 <option disabled="disabled" default="true">Select Account</option>
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
+                                <?php
+                                foreach ($result1 as $value) {
+                                    echo "<option value='" . $value['id'] . "'>" . $value['name'] . "</option>";
+                                }
+                                ?>
                             </select>
                         </div>
-                        <input class="contactInput" type="email" placeholder="Email Address"><br>
-                        <input class="contactInput" type="text" placeholder="Password"><br>
-                        <input class="contactInput" type="text" placeholder="Re type Password"><br>
-                        <input class="contactInput" type="text" placeholder="Firstname"><br>
-                        <input class="contactInput" type="text" placeholder="Lastname"><br>
-                        <select class="contactInput">
+                        <input name="email" class="contactInput" type="email" placeholder="Email Address"><br>
+                        <input name="passowrd" class="contactInput" type="text" placeholder="Password"><br>
+                        <input name="repassword" class="contactInput" type="text" placeholder="Re type Password"><br>
+                        <input name="firstname" class="contactInput" type="text" placeholder="Firstname"><br>
+                        <input name="lastname" class="contactInput" type="text" placeholder="Lastname"><br>
+                        <select name="type" class="contactInput">
                             <option hidden="true">Select Type</option>
-                            <option disabled="disabled" default="true">Select Type</option>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
+                            <?php
+                            foreach ($result2 as $value) {
+                                echo "<option value='" . $value['id'] . "'>" . $value['name'] . "</option>";
+                            }
+                            ?>
                         </select>
                         <div class="cbi">
-                            <input type="checkbox">&nbsp;
+                            <input name="termCheck" type="checkbox">&nbsp;
                             <p>By registering you agree our <b><a href="termsandcondition.php">Terms of user</a></b></p>
                         </div>
-                        <input type="submit" class="btnStyle" value="Signup">
+                        <input type="submit" name="submit" class="btnStyle" value="Signup">
                         <br>
                         <br>
                         <a href="login.php">Already have a account ?</a>
