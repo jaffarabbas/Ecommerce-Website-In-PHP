f8a8e70a293736e07e03ed74bda3aa79ff5eada7
Jaffar Abbas <gamaportal8@gmail.com>
2022-04-24 19:26:00 +0500

    24/04/2022 19:26:00

---
+++ ecommerce-clone-(psd-to-html)/vendor/js/cartSession.js
@@ -0,0 +1,27 @@
+var iprice = document.getElementsByClassName('iprice');
+var iquantity = document.getElementsByClassName('iquantity');
+var itotal = document.getElementsByClassName('totalPriceCart');
+var grandTotal = document.getElementById('grandTotal');
+
+function subTotal() {
+    var grandTotalValue = 0;
+    for (i = 0; i < iprice.length; i++) {
+        itotal[i].innerText = (iprice[i].value * iquantity[i].value);
+        grandTotalValue += parseInt(itotal[i].innerText);
+    }
+    grandTotal.innerText = grandTotalValue;
+}
+
+subTotal();
+
+
+function getPriceValues() {
+    var price = document.getElementById('ctprice');
+    var total = document.getElementById('grandTotal');
+    var tprice = document.getElementById("tprice");
+    var gtotal = document.getElementById("gtprice");
+    tprice.value = price.innerHTML;
+    gtotal.value = total.innerHTML;
+}
+
+getPriceValues();
