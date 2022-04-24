95de7f3c883de08662184489c89ef97c32a10440
Jaffar Abbas <gamaportal8@gmail.com>
2022-04-24 22:16:58 +0500

    24/04/2022 22:16:57

--- ecommerce-clone-(psd-to-html)/pages/login.php
+++ ecommerce-clone-(psd-to-html)/pages/login.php
@@ -20,11 +20,17 @@
         <div class="row">
             <div class="col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 col-xs-12">
                 <div class="auth">
-                    <form>
+                    <form action="./backend/login_backend.php" method="POST">
                         <h1>Login</h1>
-                        <input class="contactInput" type="email" placeholder="Email Address"><br>
-                        <input class="contactInput" type="text" placeholder="Password"><br>
-                        <input type="submit" class="btnStyle" value="Login">
+                        <?php
+                        if (isset($_SESSION['error']) && $_SESSION['error'] != "") {
+                            echo $_SESSION['error'];
+                            unset($_SESSION['error']);
+                        }
+                        ?>
+                        <input class="contactInput" name="email" type="email" placeholder="Email Address"><br>
+                        <input class="contactInput" name="password" type="text" placeholder="Password"><br>
+                        <input type="submit" name="login" class="btnStyle" value="Login">
                         <br>
                         <br>
                         <a href="signup.php">Create new Account</a>&nbsp;&nbsp;&nbsp;<a href="">Forget passowrd ?</a>
