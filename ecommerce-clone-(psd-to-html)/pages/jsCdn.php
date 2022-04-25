f63aa90be6db07474dbe1020a327f73617b68a27
Jaffar Abbas <gamaportal8@gmail.com>
2022-04-25 16:02:55 +0500

    25/04/2022 16:02:52

--- ecommerce-clone-(psd-to-html)/pages/jsCdn.php
+++ ecommerce-clone-(psd-to-html)/pages/jsCdn.php
@@ -1,5 +1,6 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
+<script src="../vendor/js/cartSession.js"></script>
 <script src="../vendor/js/index.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/material-ui/5.0.0-beta.5/index.js" integrity="sha512-uKxirna7d5STmVXEMQYBVRW1nERrqHjwOubv4QcK4oYaaifLiEnN/aLIJxVsyK4R1K+awpNIG73RaQfT1DZ8ew==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
