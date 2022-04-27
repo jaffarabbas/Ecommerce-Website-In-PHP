342a26e2884f6cff482e1e2e3f70339a94d83288
Jaffar Abbas <gamaportal8@gmail.com>
2022-04-27 20:15:08 +0500

    27/04/2022 20:15:07

--- ecommerce-clone-(psd-to-html)/pages/backend/login_backend.php
+++ ecommerce-clone-(psd-to-html)/pages/backend/login_backend.php
@@ -13,23 +13,25 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 $_SESSION['error'] = Component::dangerAlert("validation Message", "Please fill all Feilds");
                 Component::navigator("../login.php");
             } else {
-                $statement = $operations->queryStatement(Queries::$getUser);
-                if ($statement) {
-                    $result = mysqli_stmt_bind_param($statement, "ss", $email, $pass);
-                    mysqli_stmt_execute($statement);
-                    if ($result) {
-                        $_SESSION['user'][0] = array(
-                            'email' => $email,
-                            'password' => $pass
-                        ); 
-                        $_SESSION['success'] = Component::successAlert("Success Message", "Hii ".$email." You have successfully login");
-                        Component::navigator("../index.php");
-                    } else {
-                        $_SESSION['error'] = Component::dangerAlert("validation Message", "Something went wrong");
-                        Component::navigator("../login.php");
-                    }
+                $statement = $operations->prepareStatement(Queries::$getAdmin);
+                $statement->bind_param("ss", $name, $pass);
+                $statement->execute();
+                $result = $statement->get_result();
+                while ($row = $result->fetch_object()) {
+                    $results[] = $row;
+                }
+                if (count($results) > 0) {
+                    $_SESSION['user'][0] = array(
+                        'id' => $results[0]->id,
+                        'firstname' => $results[0]->firstname,
+                        'lastname' => $results[0]->lastname,
+                        'email' => $results[0]->email,
+                    );
+                    $_SESSION['success'] = Component::successAlert("Success Message", "Hii " . $name . " You have successfully login");
+                    Component::navigator("../index.php");
                 } else {
-                    print_r("s3");
+                    $_SESSION['error'] = Component::dangerAlert("validation Message", "Something went wrong");
+                    Component::navigator("../login.php");
                 }
             }
         } else {
