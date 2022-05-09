cddc14628b85464fb257f75635393b46bc00d0e3
Jaffar Abbas <gamaportal8@gmail.com>
2022-05-09 19:48:00 +0500

    09/05/2022 19:47:59

--- ecommerce-clone-(psd-to-html)/pages/backend/manageCart.php
+++ ecommerce-clone-(psd-to-html)/pages/backend/manageCart.php
@@ -62,20 +62,23 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             }
         }
     }
-    // if (isset($_POST['Mode_Quantity_Check_Out'])) {
-    //     foreach ($_SESSION['cart'] as $key => $value) {
-    //         if ($value['Item_id'] == $_POST['Item_id']) {
-    //             $_SESSION['cart'][$key]['Item_Quantity'] = $_POST['Mode_Quantity_Check_Out'];
-    //             foreach ($_SESSION['cart'] as $key => $value) {
-    //                 $_SESSION['cart'][$key]['Item_total_Price'] = $_SESSION['cart'][$key]['Item_Price'] * $_SESSION['cart'][$key]['Item_Quantity'];
-    //                 // $_SESSION['cart'][$key]['Item_total_Quantity'] = $_SESSION['cart'][$key]['Item_Price'] * $_SESSION['cart'][$key]['Item_Quantity'];
-    //             }
-    //             echo "<script>
-    //                     window.location.href='../checkout.php';
-    //                  </script>";
-    //         }
-    //     }
-    // }
+    if (isset($_POST['Mode_Quantity_Check_Out'])) {
+        foreach ($_SESSION['cart'] as $key => $value) {
+            if ($value['Item_id'] == $_POST['Item_id']) {
+                $_SESSION['cart'][$key]['Item_Quantity'] = $_POST['Mode_Quantity_Check_Out'];
+                $var = end($_SESSION['cart']['Item_total_Quantity']);
+                foreach ($_SESSION['cart'] as $key => $value) {
+                    $_SESSION['cart'][$key]['Item_total_Price'] = $_SESSION['cart'][$key]['Item_Price'] * $_SESSION['cart'][$key]['Item_Quantity'];
+                    // $_SESSION['cart'][$key]['Item_total_Quantity'] = 0;
+                    $var += $_SESSION['cart'][$key]['Item_total_Price'];
+                }
+                $_SESSION['cart']['Item_total_Quantity'] = $var;
+                echo "<script>
+                        window.location.href='../checkout.php';
+                     </script>";
+            }
+        }
+    }
     // set session for checkout
     if (isset($_POST['Add_to_CheckOut'])) {
         foreach ($_SESSION['cart'] as $key => $value) {
