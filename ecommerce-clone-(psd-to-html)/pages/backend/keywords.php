2f0c1ea79866d484762bab436707e70a53e8df1c
Jaffar Abbas <gamaportal8@gmail.com>
2022-05-11 12:25:35 +0500

    11/05/2022 12:25:34

--- ecommerce-clone-(psd-to-html)/pages/backend/keywords.php
+++ ecommerce-clone-(psd-to-html)/pages/backend/keywords.php
@@ -1,3 +1,7 @@
 <?php 
-
+class Messages{
+    public static string $addedToCart = "Added to cart";
+    public static string $alreadyInCart = "Item is already added in the cart";
+    public static string $removedFromCart = "Removed from cart";
+}
 ?>
