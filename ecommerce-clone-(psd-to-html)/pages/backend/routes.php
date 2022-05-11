97f6237dfe0a3a833f82bcb6d874369dc5bbb0f4
Jaffar Abbas <gamaportal8@gmail.com>
2022-05-11 11:48:18 +0500

    11/05/2022 11:48:18

--- ecommerce-clone-(psd-to-html)/pages/backend/routes.php
+++ ecommerce-clone-(psd-to-html)/pages/backend/routes.php
@@ -1,13 +1,36 @@
 <?php 
 
 class Routes{
-    public static string $indexPage = "../index.php";
-    public static string $loginPage = "../login.php";
-    public static string $signupPage = "../signup.php";
-    public static string $shopPage = "../shop.php";
-    public static string $checkoutPage = "../checkout.php";
-    public static string $contactPage = "../contact.php";
-    public static string $aboutPage = "../about.php";
+    //core pages
+    public static string $indexPage = "index.php";
+    public static string $loginPage = "login.php";
+    public static string $signupPage = "signup.php";
+    public static string $shopPage = "shop.php";
+    public static string $checkoutPage = "checkout.php";
+    public static string $contactPage = "contact.php";
+    public static string $aboutPage = "about.php";
+    public static string $productDetailsPage = "productDetails.php";
+    //reusable pages
+    public static string $headerPage = "header.php";
+    public static string $footerPage = "footer.php";
+    public static string $styleCdnPage = "styleCdn.php";
+    public static string $jsCdnPage = "jsCdn.php";
+    //backend pages
+    public static string $connecterPage = "backend/connecter.php";
+    public static string $dbConnectionPage = "backend/dbConnection.php";
+    public static string $dbOperationPage = "backend/dbOperations.php";
+    public static string $dbQueryPage = "backend/queries.php";
+    public static string $routes = "backend/routes.php";
+    public static string $keywords = "backend/keywords.php";
+    public static string $validation = "backend/validation.php";
+    public static string $methods = "backend/methods.php";
+    public static string $manageCart = "backend/manageCart.php";
+    public static string $loginBakend = "backend/login_backend.php";
+    public static string $signupBakend = "backend/signup_backend.php";
+    public static string $contactBackend = "backend/contact_backend.php";
+
+    //route parameters
+    public static string $parameter = "";
 }
 
 ?>
