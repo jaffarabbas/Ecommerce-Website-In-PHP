bc019ddedb93d11307664dccdec193238b71adf5
Jaffar Abbas <gamaportal8@gmail.com>
2022-05-11 12:25:35 +0500

    11/05/2022 12:25:34

--- ecommerce-clone-(psd-to-html)/pages/backend/manageCart.php
+++ ecommerce-clone-(psd-to-html)/pages/backend/manageCart.php
@@ -2,21 +2,8 @@
 <?php
 session_start();
 include("./methods.php");
-// function alertMaker($flag)
-// {
-//     if ($flag) {
-//         echo "<script>
-//                 alert('Added to cart');
-//                 window.location.href='../cart.php';
-//              </script>";
-//     } else {
-//         echo "
-//         <script>
-//             alert('Item is already added in the cart');
-//             window.location.href='../shop.php';
-//         </script>";
-//     }
-// }
+include("./routes.php");
+include("./keywords.php");
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     //add yo cart
@@ -25,17 +12,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             //item in array
             $item_array = array_column($_SESSION['cart'], 'Item_id');
             if (in_array($_POST['pid'], $item_array)) {
-                alertMaker(false);
+                Component::alertMaker(Routes::LinkMaker(Routes::$parameterDoubleDot,Routes::$cartPage),Messages::$alreadyInCart);
             } else {
                 //add new item to session array
                 $count_cart_index = count($_SESSION['cart']);
                 $_SESSION['cart'][$count_cart_index] = array('Item_id' => $_POST['pid'], 'Item_Name' => $_POST['name'],'Item_Description' => $_POST['description'] ,'Item_Price' => $_POST['price'], 'Item_Quantity' => $_POST['quantity'], 'Item_Image' => $_POST['image'], 'Item_cid' => $_POST['cid'],'Item_total_Price' => 0, 'Item_total_Quantity' => 0);
-                alertMaker(true);
+                Component::alertMaker(Routes::LinkMaker(Routes::$parameterDoubleDot,Routes::$cartPage),Messages::$addedToCart);
             }
         } else {
             //add first item to session array
             $_SESSION['cart'][0] = array('Item_id' => $_POST['pid'], 'Item_Name' => $_POST['name'],'Item_Description' => $_POST['description'] ,'Item_Price' => $_POST['price'], 'Item_Quantity' => $_POST['quantity'], 'Item_Image' => $_POST['image'], 'Item_cid' => $_POST['cid'],'Item_total_Price' => 0, 'Item_total_Quantity' => 0);
-            alertMaker(true);
+            Component::alertMaker(Routes::LinkMaker(Routes::$parameterDoubleDot,Routes::$cartPage),Messages::$addedToCart);
         }
     }
     //remove from cart
@@ -45,10 +32,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
                 unset($_SESSION['cart'][$key]);
                 $_SESSION['cart'] = array_values($_SESSION['cart']);
-                echo "<script>
-                        alert('Item removed from cart');
-                        window.location.href='../cart.php';
-                     </script>";
+                Component::alertMaker(Routes::LinkMaker(Routes::$parameterDoubleDot,Routes::$cartPage),Messages::$removedFromCart);
             }
         }
     }
@@ -59,9 +43,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         foreach ($_SESSION['cart'] as $key => $value) {
             if ($value['Item_id'] == $_POST['Item_id']) {
                 $_SESSION['cart'][$key]['Item_Quantity'] = $_POST['Mode_Quantity'];
-                echo "<script>
-                        window.location.href='../cart.php';
-                     </script>";
+                Component::navigator( Routes::LinkMaker(Routes::$parameterDoubleDot,Routes::$cartPage));
             }
         }
     }
@@ -72,9 +54,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 foreach ($_SESSION['cart'] as $key => $value) {
                     $_SESSION['cart'][$key]['Item_total_Price'] = $_SESSION['cart'][$key]['Item_Price'] * $_SESSION['cart'][$key]['Item_Quantity'];
                 }
-                echo "<script>
-                        window.location.href='../checkout.php';
-                     </script>";
+                Component::navigator(Routes::LinkMaker(Routes::$parameterDoubleDot,Routes::$checkoutPage));
             }
         }
     }
@@ -84,9 +64,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $_SESSION['cart'][$key]['Item_total_Price'] = $_SESSION['cart'][$key]['Item_Price'] * $_SESSION['cart'][$key]['Item_Quantity'];
             $_SESSION['cart'][$key]['Item_total_Quantity'] = $_POST['grandTotalPrice'];
         }
-        echo "<script>
-                window.location.href='../checkout.php';
-              </script>";
+        Component::navigator(Routes::LinkMaker(Routes::$parameterDoubleDot,Routes::$checkoutPage));
     }
 }
 
