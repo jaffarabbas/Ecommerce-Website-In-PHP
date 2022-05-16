c803c9972a8bcfbb4d4a011d99d852cc21db846e
Jaffar Abbas <gamaportal8@gmail.com>
2022-05-16 19:06:29 +0500

    16/05/2022 19:06:28

--- ecommerce-clone-(psd-to-html)/pages/backend/logout.php
+++ ecommerce-clone-(psd-to-html)/pages/backend/logout.php
@@ -5,6 +5,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_POST['logout'])) {
     session_unset();
     session_destroy();
-    Component::alertMaker("../index.php","Do you wnat to logout");
+    Component::alertMaker("../index.php",Messages::$logout);
     }
 }
