5e3861c98a08dd65522a9217f1d51440b3226843
Jaffar Abbas <gamaportal8@gmail.com>
2022-05-16 19:06:29 +0500

    16/05/2022 19:06:28

--- ecommerce-clone-(psd-to-html)/pages/backend/keywords.php
+++ ecommerce-clone-(psd-to-html)/pages/backend/keywords.php
@@ -3,5 +3,10 @@ class Messages{
     public static string $addedToCart = "Added to cart";
     public static string $alreadyInCart = "Item is already added in the cart";
     public static string $removedFromCart = "Removed from cart";
+    public static string $logout = "Do you want to logout";
+}
+
+class ValidationMessages{
+    
 }
 ?>
