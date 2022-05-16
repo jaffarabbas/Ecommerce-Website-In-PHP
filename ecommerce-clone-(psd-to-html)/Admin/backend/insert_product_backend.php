4060959e011dd8b329b5c364282c7df80f5d048b
Jaffar Abbas <gamaportal8@gmail.com>
2022-05-16 19:06:29 +0500

    16/05/2022 19:06:28

--- ecommerce-clone-(psd-to-html)/Admin/backend/insert_product_backend.php
+++ ecommerce-clone-(psd-to-html)/Admin/backend/insert_product_backend.php
@@ -4,9 +4,32 @@ include("../../pages/backend/connecter.php");
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_POST['upload'])) {
-        $images = $_FILES['images'];
-        print_r($images);
+        foreach ($_FILES['images']['tmp_name'] as $key => $value) {
+            $filename=$_FILES['images']['name'][$key];
+            $filename_tmp=$_FILES['images']['tmp_name'][$key];
+            echo '<br>';
+            $fileExtension=pathinfo($filename,PATHINFO_EXTENSION);
+            $finalimg='';
+            if(in_array($fileExtension,Validation::$extension))
+            {
+                if(!file_exists('images/'.$filename))
+                {
+                    move_uploaded_file($filename_tmp, 'images/'.$filename);
+                    $finalimg=$filename;
+                }else{
+                     $filename=str_replace('.','-',basename($filename,$fileExtension));
+                     $newfilename=$filename.time().".".$fileExtension;
+                     move_uploaded_file($filename_tmp, 'images/'.$newfilename);
+                     $finalimg=$newfilename;
+                }
+                echo $finalimg;
+                // $_SESSION['success'] = Component::successAlert("Success Message", "You have successfully registered");
+                // Component::navigator("../signup.php");
+            }else
+            {
+                $_SESSION['error'] = Component::dangerAlert("validation Message", "Something went wrong");
+                Component::navigator("../insert_products.php");
+            }
+        }
     }
 }
-
-?>
