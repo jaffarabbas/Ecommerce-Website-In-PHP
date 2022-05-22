b6ab01f9757e50c28ee8a7384b432bd0222fd4ce
Jaffar Abbas <gamaportal8@gmail.com>
2022-05-22 21:41:52 +0500

    22/05/2022 21:41:44

--- ecommerce-clone-(psd-to-html)/Admin/backend/insert_product_backend.php
+++ ecommerce-clone-(psd-to-html)/Admin/backend/insert_product_backend.php
@@ -1,35 +1,62 @@
 <?php 
 
 include("../../pages/backend/connecter.php");
+session_start();
+$validation = new Validation();
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_POST['upload'])) {
-        foreach ($_FILES['images']['tmp_name'] as $key => $value) {
-            $filename=$_FILES['images']['name'][$key];
-            $filename_tmp=$_FILES['images']['tmp_name'][$key];
-            echo '<br>';
-            $fileExtension=pathinfo($filename,PATHINFO_EXTENSION);
-            $finalimg='';
-            if(in_array($fileExtension,Validation::$extension))
-            {
-                if(!file_exists('images/'.$filename))
+        $name = $validation->validate(($operations->escape_string($_POST['name'])));
+        $description = $validation->validate(($operations->escape_string($_POST['description'])));
+        $price = $validation->validate(($operations->escape_string($_POST['price'])));
+        $quantity = $validation->validate(($operations->escape_string($_POST['quantity'])));
+        $categories = $validation->validate(($operations->escape_string($_POST['categories'])));
+        $images = $_FILES['images'];
+        $imageArray = [];
+        $checkFeilds = $validation->checkEmpty($_POST,array('name','description','price','quantity','categories'));
+        if ($checkFeilds) {
+            $_SESSION['error'] = Component::dangerAlert("validation Message", "Please fill all the fields");
+            Component::navigator("../insert_products.php");
+        }else{
+            foreach ($_FILES['images']['tmp_name'] as $key => $value) {
+                $filename=$_FILES['images']['name'][$key];
+                $filename_tmp=$_FILES['images']['tmp_name'][$key];
+                $fileExtension=pathinfo($filename,PATHINFO_EXTENSION);
+                $finalimg='';
+                if(in_array($fileExtension,Validation::$extension))
                 {
-                    move_uploaded_file($filename_tmp, 'images/'.$filename);
-                    $finalimg=$filename;
-                }else{
-                     $filename=str_replace('.','-',basename($filename,$fileExtension));
-                     $newfilename=$filename.time().".".$fileExtension;
-                     move_uploaded_file($filename_tmp, 'images/'.$newfilename);
-                     $finalimg=$newfilename;
+                    if(!file_exists(Routes::LinkMaker(Routes::$parameterDoubleDot,Routes::$imagesFolder).$filename))
+                    {
+                        move_uploaded_file($filename_tmp, Routes::LinkMaker(Routes::$parameterDoubleDot,Routes::$imagesFolder).$filename);
+                        $finalimg=$filename;
+                        array_push($imageArray,Routes::LinkMaker(Routes::$parameterDoubleDot,Routes::$backendImageToShop.$finalimg));
+                    }else{
+                        $filename=str_replace('.','-',basename($filename,$fileExtension));
+                        $newfilename=$filename.time().".".$fileExtension;
+                        move_uploaded_file($filename_tmp, Routes::LinkMaker(Routes::$parameterDoubleDot,Routes::$imagesFolder).$newfilename);
+                        $finalimg=$newfilename;
+                        array_push($imageArray,Routes::LinkMaker(Routes::$parameterDoubleDot,Routes::$backendImageToShop.$finalimg));
+                    }
+                }else
+                {
+                    $_SESSION['error'] = Component::dangerAlert("validation Message", "Something went wrong");
+                    Component::navigator("../insert_products.php");
                 }
-                echo $finalimg;
-                // $_SESSION['success'] = Component::successAlert("Success Message", "You have successfully registered");
-                // Component::navigator("../signup.php");
-            }else
-            {
-                $_SESSION['error'] = Component::dangerAlert("validation Message", "Something went wrong");
-                Component::navigator("../insert_products.php");
+            }
+            $statement = $operations->queryStatement(Queries::$insertProduct);
+            if ($statement) {
+                $imgString = '["'.implode('","',$imageArray).'"]';
+                mysqli_stmt_bind_param($statement, "ssdsii", $name, $description, $price,$imgString,$quantity, $categories);
+                $result = mysqli_stmt_execute($statement);
+                if ($result) {
+                    $_SESSION['success'] = Component::successAlert("Success Message", "Product Inserted Successfully");
+                    Component::navigator("../insert_products.php");
+                } else {
+                    $_SESSION['error'] = Component::dangerAlert("validation Message", "Something went wrong");
+                    Component::navigator("../insert_products.php");
+                }
             }
         }
+
     }
 }
