baa2227a35939462dc0967c5f890f0c606e4d8a3
Jaffar Abbas <gamaportal8@gmail.com>
2022-06-06 22:18:35 +0500

    06/06/2022 22:18:27

--- ecommerce-clone-(psd-to-html)/Admin/login.php
+++ ecommerce-clone-(psd-to-html)/Admin/login.php
@@ -1,3 +1,8 @@
+<?php 
+
+session_start();
+
+?>
 <!DOCTYPE html>
 <html>
 
@@ -14,9 +19,15 @@
 	</div>
 	<form action="./backend/login_backend.php" method="post">
 		<h2>LOGIN</h2>
-		<?php if (isset($_GET['error'])) { ?>
-			<p class="error"><?php echo $_GET['error']; ?></p>
-		<?php } ?>
+		<?php
+		if (isset($_SESSION['error']) && $_SESSION['error'] != "") {
+			echo $_SESSION['error'];
+			unset($_SESSION['error']);
+		} elseif (isset($_SESSION['success']) && $_SESSION['success'] != "") {
+			echo $_SESSION['success'];
+			unset($_SESSION['success']);
+		}
+		?>
 		<label>User Name</label>
 		<input type="text" name="name" placeholder="User Name"><br>
 
