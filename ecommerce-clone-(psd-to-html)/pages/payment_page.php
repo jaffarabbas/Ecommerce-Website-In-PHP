d8b860eb37e37e28f4b2ddc47f17712025e796a5
Jaffar Abbas <gamaportal8@gmail.com>
2022-06-07 10:44:38 +0500

    07/06/2022 10:44:37

---
+++ ecommerce-clone-(psd-to-html)/pages/payment_page.php
@@ -0,0 +1,27 @@
+<?php include("header.php") ?>
+    <!-- banner start -->
+    <section class="about_banner">
+        <div class="container">
+            <!-- col-sm-6 col-sm-offset-2 col-xs-6 col-xs-offset-1 -->
+            <div class="row mini_banner_row">
+                <div class="col-md-6 col-md-offset-3">
+                    <div class="mini_banner_center text-center">
+                        <h1>Payment Page</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- banner end -->
+
+    <!-- about start -->
+    <section class="about">
+        <div class="container">
+            <div class="row">
+                
+            </div>
+        </div>
+    </section>
+    <!-- about end -->
+<!-- contact end -->
+<?php include("footer.php") ?>
