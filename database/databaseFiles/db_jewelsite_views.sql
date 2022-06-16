c80f00f79c7355a4bf2a0924035b34480c545cff
Jaffar Abbas <gamaportal8@gmail.com>
2022-06-16 14:35:34 +0500

    16/06/2022 14:35:32

--- database/databaseFiles/db_jewelsite_views.sql
+++ database/databaseFiles/db_jewelsite_views.sql
@@ -1,6 +1,10 @@
 CREATE VIEW SHOW_USER_ORDER_ALL_DATA
 AS
-SELECT u.*,p.*,uo.oid , uo.uid , uo.iid , uo.quantity as order_quantity , uo.total_price , uo.status
- , uo.created_at FROM user_orders uo
-INNER JOIN products p ON uo.iid = p.pid
-INNER JOIN users u ON uo.uid = u.id;
+SELECT  uo.oid ,u.id as user_id,concat(u.firstname ,'_', u.lastname) as user_name,u.email,u.status as user_status , u.created_at as user_created,
+        uo.uid ,uo.iid , uo.quantity as order_quantity , uo.total_price , uo.status as uo_status ,uo.created_at as order_created_at,
+        p.pid as product_id , p.name , p.image ,p.price , p.quantity as inventory , p.cid as category , p.status as product_status , p.created_on as product_created_at
+FROM user_orders uo
+        INNER JOIN products p ON uo.iid = p.pid
+        INNER JOIN users u ON uo.uid = u.id;
+
+select * from show_user_order_all_data
