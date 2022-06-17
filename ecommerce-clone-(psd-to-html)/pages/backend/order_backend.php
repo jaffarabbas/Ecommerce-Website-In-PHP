87fa6dfddf8a9030a9ec264dfd196a6c9356f0d7
Jaffar Abbas <gamaportal8@gmail.com>
2022-06-17 12:30:27 +0500

    17/06/2022 12:30:25

--- ecommerce-clone-(psd-to-html)/pages/backend/order_backend.php
+++ ecommerce-clone-(psd-to-html)/pages/backend/order_backend.php
@@ -6,12 +6,13 @@ session_start();
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_POST['Check_Out']) || isset($_SESSION['order_items']) || $_SESSION['order_items'] != null) {
         foreach ($_SESSION['order_items'] as $key => $value) {
-            $uid = $value['user'];
+            $token = Component::tokenGenerator();
+            $_SESSION['user'] != null ? $uid = $value['user'] : $name = $value['name']; $email = $value['email']; $phone = $value['phone']; $address = $value['address'];
             $pid = $value['Item_id'];
             $Item_Quantity = $value['Item_Quantity'];
             $total_price = $value['total_price'];
-            $statement = $operations->queryStatement(Queries::$insertUsersOrders);
-            $statement->bind_param("iiii", $uid, $pid, $Item_Quantity, $total_price);
+            $statement = $operations->queryStatement($_SESSION['user'] != null ? Queries::$insertUsersOrders : Queries::$insertTempUsersOrders);
+            $_SESSION['user'] != null ? $statement->bind_param("iiid", $uid, $pid, $Item_Quantity, $total_price) : $statement->bind_param("sssssiid",$token ,$name, $email, $phone, $address, $pid, $Item_Quantity, $total_price);
             $result = mysqli_stmt_execute($statement);
         }
         if ($statement) {
