3e4b17655bd28a71ecb88c3ed2df7559f5f85a1c
Jaffar Abbas <gamaportal8@gmail.com>
2022-06-19 17:00:24 +0500

    19/06/2022 17:00:23

---
+++ database/db_jewelsite_categories.sql
@@ -0,0 +1,3 @@
+INSERT INTO db_jewelsite.categories (cid, cname, status) VALUES (1, 'latest Products', 1);
+INSERT INTO db_jewelsite.categories (cid, cname, status) VALUES (2, 'Our Premium', 1);
+INSERT INTO db_jewelsite.categories (cid, cname, status) VALUES (3, 'Artifical Products', 1);
